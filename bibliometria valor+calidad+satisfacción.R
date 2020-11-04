c4b15ee3256966010158367e48098d150a7decbd
ALRIER <56406907+psicorivera4@users.noreply.github.com>
2020-11-04 13:51:46 -0500

    Update bibliometria valor+calidad+satisfacción.R

--- bibliometria valor+calidad+satisfacción.R
+++ bibliometria valor+calidad+satisfacción.R
@@ -27,12 +27,20 @@ setwd("/home/alrier/Documentos/bibliometrías")
 file <- ("/home/alrier/Documentos/bibliometrías/valor y calidad bibliometria/scopus.bib")
 #convierto a Dataframe
 M <- convert2df(file, dbsource = "scopus", format = "bibtex")
+
+#'''Paso los documentos a formato tibble para trabajar un filtro 
+#por años a partir del año 2018 en adelante, pero es solo para hacer
+#un análisis más liviano, después puedo retomar el objeto M que es un 
+#DF y contiene todos los resultados''' 
+#N1 <- as.tibble(M)
+#N2<- N1 %>% filter(PY >=2018)
+#convierto nuevamente a DF para continuar trabajando sobre mi filtro
+#M1 <- as.data.frame(N2)
 #agrupo y resumo resultados
 '''este sumario y los plots es mejor trabajarlos sobre el archivo 
 bibliometrix en formato DF'''
 S1 <- biblioAnalysis(M, sep = ";")
 S2 <- summary(object = S1, k = 20, pause = FALSE)
-#Genero un plot de los biblioanalizables
 plot(x = S1, k = 20, pause = FALSE)
 
 '''genero la primera red matricial de datos'''
@@ -41,22 +49,23 @@ NetMatrix <- biblioNetwork(M, analysis = "co-occurrences", network = "author_key
 a partir de 10 mil observaciones requiere buena RAM del equipo'''
 P <- normalizeSimilarity(NetMatrix, type = "association")
 '''Grafico mi primera red'''
-p1net <- networkPlot(P, n = 20, Title = "co-occurrence network", type = "fruchterman", 
+p1net <- networkPlot(P, n = 15, Title = "co-occurrence network", type = "fruchterman", 
                          labelsize = 1, size = 10, size.cex = T, halo = T, cluster = "walktrap",
                          remove.isolates = F, curved = 0.9, edgesize = 3,remove.multiple = T, noloops = T, weighted = TRUE)
 
 '''segunda red matricial de datos con su grafica'''
-NetMatrix2 <- biblioNetwork(M, analysis = "co-citation", network = "references", sep = ". ")
+NetMatrix2 <- biblioNetwork(M, analysis = "co-citation", network = "references", sep = ";")
 '''Grafico mi segunda red''' 
-p2net=networkPlot(NetMatrix2, n = 10, Title = "Country Collaboration", type = "fruchterman", labelsize = 1, size = 10, size.cex = T, halo = T, cluster = "spinglass",
-                remove.isolates = T, curved = 0.9, edgesize = 3,remove.multiple = T, noloops = T, weighted = TRUE)
+p2net=networkPlot(NetMatrix2, n = 15, Title = "co-citation", type = "fruchterman", labelsize = 1, size = 10, size.cex = T, halo = F, cluster = "leading_eigen",
+                remove.isolates = F, curved = 0.9, edgesize = 3,remove.multiple = T, noloops = T, weighted = TRUE)
+
 
 '''Genero una tercera red matricial de datos'''
 n <- metaTagExtraction(M, Field = "AU_CO", sep = ";")
 NetMatrix3 <- biblioNetwork(n, analysis = "collaboration", network = "countries", sep = ";")
 '''ploteo la red'''
-p3net=networkPlot(NetMatrix3, n = 10, Title = "Country Collaboration", type = "fruchterman", labelsize = 1, size = 10, size.cex = T, halo = T, cluster = "spinglass",
-                remove.isolates = T, curved = 0.9, edgesize = 3,remove.multiple = T, noloops = T, weighted = TRUE)
+p3net=networkPlot(NetMatrix3, n = 20, Title = "Country Collaboration", type = "fruchterman", labelsize = 1, size = 10, size.cex = T, halo = T, cluster = "spinglass",
+                remove.isolates = T, curved = 0.9, edgesize = 3,remove.multiple = F, noloops = T, weighted = TRUE)
 
 #Del total de resultados, extraigo los papers más citados
 '''los puedo retirar del sumario o del total de observaciones'''
@@ -71,7 +80,7 @@ View(AU)
 
 '''hago lo mismo con los países'''
 Paises <- S1$Countries
-Paises <- S1$MostProdCountries
+Paises <- S2$MostProdCountries
 View(Paises)
 Paises <- Paises[c(1, 3)]
 '''re nombro la columna paises'''
@@ -81,12 +90,12 @@ names(Paises)[1] <- "Country"
 '''quito algonos errores'''
 Paises$Freq <- suppressWarnings(as.numeric(Paises$Freq))
 '''miro los años de producción'''
-Produccion <- S1$AnnualProduction
+Produccion <- S2$AnnualProduction
 names(Produccion)[1] <- "Year"
 '''seteo la segunda columna como numerica'''
 Produccion$Articles <- as.numeric(Produccion$Articles)
 '''revisemos los keywords'''
-key<- S1$MostRelKeywords
+key<- S2$MostRelKeywords
 key <- key[c(1, 2)]
 plot(key)
 '''graficas y plots'''
@@ -94,9 +103,14 @@ Fig1A <- ggplot(Paises, aes(x=reorder(Country, Freq) , y=Freq)) + geom_bar(stat 
 Fig1B <- ggplot(Produccion, aes(x=Year , y=Articles)) + geom_bar(stat = "identity", fill="blue") + xlab("Year") + ylab("Articles") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 ggarrange (Fig1A, Fig1B, labels = c("A", "B"), ncol = 2, nrow = 1)
 
-#este trabajo de minería se hace sobre el total de observaciones S1.
+'''graficas y plots por citas y palabras clave'''
+Fig1A <- ggplot(key, aes(x=1 , y=2)) + geom_bar(stat = "identity", fill="blue") + coord_flip() + xlab("KeyWords") + ylab("Articles")
+Fig1B <- ggplot(AUT, aes(x=2 , y=1)) + geom_bar(stat = "identity", fill="blue") + xlab("Paper") + ylab("TotalC") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+ggarrange (Fig1A, Fig1B, labels = c("A", "B"), ncol = 2, nrow = 1)
+plot(Fig1A)
+#este trabajo de minería se hace sobre el total de observaciones N3.
 '''Minería de datos'''
-texto = Corpus(VectorSource(S1)) 
+texto = Corpus(VectorSource(S2$MostRelKeywords)) 
 '''minuscula (A!=a)'''
 discurso=tm_map(texto, tolower)
 '''quitamos los espacios en blanco'''
@@ -110,13 +124,13 @@ discurso = tm_map(discurso, removeNumbers)
 '''quitamos palabras genericas y todas las que 
 molesten en el resultado. PDT: hay que cambiar la lista de 
 palabras que es de otra bibliometría'''
-discurso=tm_map(discurso, removeWords, c(stopwords("english"), "zhang", "human", "usa", "kingdom", "korea", "institute", "national", "lee", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "chen", "kong", "hong", "infectious", "animal", "wang", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "liu", "clinical", "chinese", "van", "department", "affiliated", "united", "universit", "kim", "viral", "california", "vaccine"))
+discurso=tm_map(discurso, removeWords, c(stopwords("english"), "behavioral","article","cperceived","zhang", "human", "usa", "kingdom", "korea", "institute", "national", "lee", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "chen", "kong", "hong", "infectious", "animal", "wang", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "liu", "clinical", "chinese", "van", "department", "affiliated", "united", "universit", "kim", "viral", "california", "vaccine"))
 
 ############### DATA FRAME DE PALABRAS CON SU FRECUENCIA
 
 '''Creamos matriz de letras'''
 letras= TermDocumentMatrix(discurso)
-findFreqTerms(letras, lowfreq=5)
+findFreqTerms(letras, lowfreq=6)
 matrix=as.matrix(letras)
 
 '''lo ordenamos y sumamos las letras de nuestra matriz'''
@@ -126,21 +140,21 @@ dataletras <- data.frame(word= names(vector),frequencia=vector)
 
 ################ GRAFICAMOS FRECUENCIA DE LAS PALABRAS
 
-barplot(dataletras[1:30,]$freq, las = 2, names.arg = dataletras[1:30,]$word,
-        col ="blue", main ="PALABRAS MÁS FRECUENTES", ylab = "Frecuencia de palabras")
+barplot(dataletras[1:15,]$freq, las = 2, names.arg = dataletras[1:15,]$word,
+        col ="blue", main ="Most Relevant Keywords", ylab = "Frecuencia de palabras")
 
 
 ############ GRAFICAMOS LA NUBE DE PALABRAS
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 2,
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1,
           max.words=70)
 '''organizar la nube de palabras'''
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 5,
-          max.words=150, random.order=FALSE, rot.per=0.2, 
-          colors=brewer.pal(7, "Dark2"))
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1,
+          max.words=150, random.order=FALSE, rot.per=0.1, 
+          colors=brewer.pal(1, "Dark2"))
 
 #Este trabajo de minería se hace sobre los keyWords
 '''Minería de datos'''
-texto = Corpus(VectorSource(key)) 
+texto = Corpus(VectorSource(S2$)) 
 '''minuscula (A!=a)'''
 discurso=tm_map(texto, tolower)
 '''quitamos los espacios en blanco'''
@@ -154,13 +168,13 @@ discurso = tm_map(discurso, removeNumbers)
 '''quitamos palabras genericas y todas las que 
 molesten en el resultado. PDT: hay que cambiar la lista de 
 palabras que es de otra bibliometría'''
-discurso=tm_map(discurso, removeWords, c(stopwords("english"), "zhang", "human", "usa", "kingdom", "korea", "institute", "national", "lee", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "chen", "kong", "hong", "infectious", "animal", "wang", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "liu", "clinical", "chinese", "van", "department", "affiliated", "united", "universit", "kim", "viral", "california", "vaccine"))
+discurso=tm_map(discurso, removeWords, c(stopwords("english"), "human", "usa", "kingdom", "korea", "institute", "national", "china", "univ", "cell", "london", "medical","coronavirus", "gene", "kong", "hong", "infectious", "animal", "diseases", "veterinary", "center", "centre", "college", "sciences", "school", "protein", "public", "control", "state", "clinical", "chinese", "department", "affiliated", "united", "universit", "viral", "california", "vaccine"))
 
 ############### DATA FRAME DE PALABRAS CON SU FRECUENCIA
 
 '''Creamos matriz de letras'''
 letras= TermDocumentMatrix(discurso)
-findFreqTerms(letras, lowfreq=5)
+findFreqTerms(letras, lowfreq=1)
 matrix=as.matrix(letras)
 
 '''lo ordenamos y sumamos las letras de nuestra matriz'''
@@ -175,10 +189,10 @@ barplot(dataletras[1:30,]$freq, las = 2, names.arg = dataletras[1:30,]$word,
 
 
   ############ GRAFICAMOS LA NUBE DE PALABRAS
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 2,
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1,
           max.words=70)
 '''organizar la nube de palabras'''
-wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 5,
+wordcloud(words = dataletras$word, freq = dataletras$freq, min.freq = 1,
           max.words=150, random.order=FALSE, rot.per=0.2, 
           colors=brewer.pal(7, "Dark2"))
 
